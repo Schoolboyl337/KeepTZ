e574a0a40e348457699a40b7492ee0c320a9abcd
Schoolboy1337 <sergeigorobtsev@gmail.com>
2023-07-04 04:00:44 +0400

    feat: End UserInfo block

--- KeepCode/src/components/VUserInfo.vue
+++ KeepCode/src/components/VUserInfo.vue
@@ -11,27 +11,66 @@
         </div>
         <div class="info-container-left-block">
           <div class="socials">
-            <a href="#">
-              <img src="/userInfo/social-logo.svg" alt="social" />
-            </a>
-            <a href="#">
-              <img src="/userInfo/social-logo.svg" alt="social" />
-            </a>
-            <a href="#">
+            <a v-for="index in 3" :key="`index-${index}`" href="#">
               <img src="/userInfo/social-logo.svg" alt="social" />
             </a>
           </div>
           <a href="#" class="info-container-left-comments">Коментарии</a>
         </div>
       </div>
-      <div>
-        <img src="/userInfo/avatar.png" alt="avatar" />
+      <div class="info-container-status">
+        <div class="info-container-right-block-text title">Статус</div>
+        <div class="block">
+          <div class="figura" />
+          <div class="info-container-right-block-text">Активен</div>
+        </div>
+      </div>
+      <div class="info-container-right">
+        <div class="info-container-right-block">
+          <div v-for="item in fakeData" :key="item.id">
+            <div class="info-container-right-block-text title">
+              {{ item.title }}
+            </div>
+            <div class="info-container-right-block-text">{{ item.text }}</div>
+          </div>
+        </div>
+      </div>
+      <div class="info-container-buttons">
+        <a href="#">
+          <img src="/edit.svg" alt="icon" />
+        </a>
+        <a href="#">
+          <img src="/bin.svg" alt="icon" />
+        </a>
       </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const fakeData = [
+  {
+    id: 1,
+    title: "Телефон",
+    text: "8-900-000-00-00",
+  },
+  {
+    id: 2,
+    title: "Дата рождения",
+    text: "25.04.2004 (17 лет)",
+  },
+  {
+    id: 3,
+    title: "Телефон Родителей",
+    text: "8-900-000-00-00",
+  },
+  {
+    id: 4,
+    title: "Адрес",
+    text: "г. Краснодар, ул. Советская 24, кв. 208",
+  },
+];
+</script>
 
 <style lang="scss" scoped>
 .info {
@@ -44,7 +83,7 @@
     padding: 20px;
 
     display: flex;
-    gap: 40px;
+    justify-content: space-between;
 
     &-left {
       display: flex;
@@ -75,6 +114,47 @@
         font-size: 36px;
         font-weight: 700;
       }
+    }
+
+    &-status {
+      align-self: end;
+      display: flex;
+      flex-direction: column;
+      & .block {
+        display: flex;
+        gap: 9px;
+        align-items: center;
+        & .figura {
+          width: 9px;
+          height: 9px;
+          border-radius: 2px;
+          background: #9ecb45;
+        }
+      }
+    }
+
+    &-right {
+      display: flex;
+      &-block {
+        grid-template-columns: 1fr 1fr;
+        display: grid;
+        align-content: space-between;
+
+        &-text {
+          color: #0d2839;
+          font-size: 14px;
+          font-weight: 400;
+          &.title {
+            color: #86939c;
+          }
+        }
+      }
+    }
+
+    &-buttons {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
     }
   }
 }
